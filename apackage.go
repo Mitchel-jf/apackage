eb794cf52236c5b60ee5dd83b7abdabc7c1d5966
gagner-team <care.gagner@gmail.com>
2021-01-18 15:51:06 +0100

    Added documnetation

--- apackage.go
+++ apackage.go
@@ -1,7 +1,9 @@
+// Pacakge apackage tests how go packages work
 package apackage
 
 import "fmt"
 
+// Hello prints the message "Hello Package" to the console
 func Hello(){
 	fmt.Println("Hello Package")
 }
--- deutsch/deutsch.go
+++ deutsch/deutsch.go
@@ -1,11 +1,14 @@
+//Package deutsch says greetings in deutsch
 package deutsch
 
 import "fmt"
 
+// Hallo prints the message "Hallo!" to the console
 func Hallo(){
 	fmt.Println("Hallo!")
 }
 
+// GutenTag prints the message "Guten Tag!" to the console
 func GutenTag(){
 	fmt.Println("Guten Tag!")
 }
